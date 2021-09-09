7609eef7da3bbc777dd398ff142541d4e245b248
zaidbaba96 <68944150+zaidbaba96@users.noreply.github.com>
2021-09-09 16:38:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const express = require('express');
 const app= express();
 require("dotenv").config();
 const port = process.env.PORT || 4000;
-
+const cors = require('cors')
 
 app.use(express.json())
 
 const cookieParser = require("cookie-parser")
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 // app.use(function (req, res, next) {
 //   /*var err = new Error('Not Found');
 //    err.status = 404;
