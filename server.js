97ea67adfe4fa30745dcb2f82c1e3a354b515078
zaidbaba96 <68944150+zaidbaba96@users.noreply.github.com>
2021-09-13 17:26:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,9 @@ const cors = require('cors')
 app.use(express.json())
 
 const cookieParser = require("cookie-parser")
-app.use(function (req, res, next) {
-
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 // app.use(function (req, res, next) {
