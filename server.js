63ff15856a0c7ae798ba214720ec1917c8a1a958
zaidbaba96 <68944150+zaidbaba96@users.noreply.github.com>
2021-09-13 17:31:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const cors = require('cors')
 app.use(express.json())
 
 const cookieParser = require("cookie-parser")
+
+app.use(cors());
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
 });
 // app.use(function (req, res, next) {
 //   /*var err = new Error('Not Found');
